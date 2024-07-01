675ea80731f13ea89bc204d2abdcbc4339985c11
HYUNWOO\hae96 <gusdn200@gmail.com>
2024-07-01 19:58:12 +0900

    NetworkManager Listener추가 asmdef 변경

--- Assets/02.Scripts/Network/NetworkManager.cs
+++ Assets/02.Scripts/Network/NetworkManager.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 using Game.DesignPattern;
-using Unity.Collections;
-using System.Net;
 using Unity.NetCode;
 using Unity.Networking.Transport;
+using System;
 namespace Game.Network
 {
     public enum NetworkConnectingType {
@@ -16,9 +15,11 @@ namespace Game.Network
         [SerializeField] private string _ip;
         [SerializeField] private ushort _port;
         [SerializeField] private NetworkConnectingType _networkConnectingType;
-
+        public Action loadListner = null;
+        
         public string Ip { get { return _ip; } }
         public ushort Port { get { return _port; } }
+
         public NetworkConnectingType NetworkConnectingType {
             get { return _networkConnectingType; }
         }
@@ -34,12 +35,14 @@ namespace Game.Network
             _networkConnectingType = NetworkConnectingType.Client;
             NetworkEndpoint endPoint = NetworkEndpoint.Parse(_ip, _port);
             ClientServerBootstrap.DefaultConnectAddress = endPoint;
-         
+            ClientServerBootstrap.AutoConnectPort = _port;
+            loadListner?.Invoke();
         }
 
         public void LoadServerClient() {
             _networkConnectingType = NetworkConnectingType.ServerClient;
             ClientServerBootstrap.AutoConnectPort = _port;
+            loadListner?.Invoke();
         }
 
     }
